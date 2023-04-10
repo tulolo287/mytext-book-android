edb6ddb310b713f79c7489a5d205726ffcd613ad
tulolo287 <iv1055@yandex.ru>
2023-04-10 16:20:57 +0300

    dgbhgfs

--- app/src/main/java/com/example/mytextbook/Activity/IntroActivity.kt
+++ app/src/main/java/com/example/mytextbook/Activity/IntroActivity.kt
@@ -1,12 +1,21 @@
 package com.example.mytextbook.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import com.example.mytextbook.R
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        val btn_go = findViewById<Button>(R.id.btn_go)
+        btn_go.setOnClickListener(View.OnClickListener {
+            startActivity(Intent(this@IntroActivity, MainActivity::class.java))
+        })
     }
 }
